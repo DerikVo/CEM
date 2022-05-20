10dff7ffb92fb2ada2d04031e446f795636a2f95
DerikVo <derikvosanjose@gmail.com>
2022-05-20 11:56:34 -0700

    Update CertificateQueries.sql

--- CertificateQueries.sql
+++ CertificateQueries.sql
@@ -1,4 +1,4 @@
-/* Zoom certificate query */
+--Zoom certificate query
 
 SELECT 
 	DISTINCT ("Parent Information"), "Parent Name"
@@ -17,6 +17,27 @@ From
 WHERE 
  	"Name of Class" IN ('Google Docs Practice Session' , 'Google Drive Practice Session', 'Google Drive-1', 'Google Drive-2', 'Google Drive-1, Google Drive-2', 'Google Drive-3 Pt. 1', 
  	'Google Drive-3 Pt. 2', 'Google Drive-4') 
+ORDER BY
+	"Parent Information" DESC
+--Typing Certificate
+
+SELECT 
+	DISTINCT ("Parent Information"), "Parent Name"
+From
+	R_certificate_csv rcc 
+WHERE 
+ 	"Name of Class" IN ('Typing-1') 
+ORDER BY
+	"Parent Information" DESC
+
+--linkedIN certificate
+
+SELECT 
+	DISTINCT ("Parent Information"), "Parent Name"
+From
+	R_certificate_csv rcc 
+WHERE 
+ 	"Name of Class" IN ('LinkedIn-1', 'LinkedIn-2') 
 ORDER BY
 	"Parent Information" DESC
 	
