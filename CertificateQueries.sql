ebcc3714091bedc31fb5a1bebd9e9daddb364d17
DerikVo <derikvosanjose@gmail.com>
2022-05-27 12:16:21 -0700

    Added Email to report
    have a 'receptive to email' in WHERE clause to only pull parents who are comfortable with being emailed. Email will allow us to send new certificates to parents

--- CertificateQueries.sql
+++ CertificateQueries.sql
@@ -1,19 +1,22 @@
+
 --Pulls Zoom Certificate
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Zoom Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Zoom Certificate"
 From
 	R_certificate_csv rc
 	JOIN
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE 
  	"Name of Class" IN ("Zoom-1", "Zoom-2")
+ 	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count(DISTINCT("Name of Class")) >= 2;
 --Pulls Google Drive Certificate
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Google Drive Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Google Drive Certificate"
 From
 	R_certificate_csv rc
 	JOIN
@@ -21,52 +24,60 @@ From
 WHERE 
  	"Name of Class" IN ('Google Docs Practice Session' , 'Google Drive Practice Session', 'Google Drive-1', 'Google Drive-2', 'Google Drive-3 Pt. 1' , 'Google Drive-3 Pt. 1', 
  	'Google Drive-3 Pt. 2', 'Google Drive-4')
+ AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count(DISTINCT("Name of Class")) >= 7;
 --Pulls Typing Certificate
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Typing Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Typing Certificate"
 From
 	R_certificate_csv rc
 	JOIN
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE 
  	"Name of Class" IN ('Typing-1')
+ 	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count(DISTINCT("Name of Class")) >= 1;
 --Pulls LinkedIn Certificate
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "LinkedIn Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "LinkedIn Certificate"
 From
 	R_certificate_csv rc
 	JOIN
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE
 	"Name of Class" IN ('LinkedIn-1', 'LinkedIn-2')
+	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count("Name of Class") >= 2;
 --Pulls Math Certificate
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Math Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Math Certificate"
 From
 	R_certificate_csv rc
 	JOIN
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE
 	"Name of Class" IN ('Math-1 pt. 1', 'Math-1 pt. 2', 'Math-2 pt. 1', 'Math-1 pt. 2')
+	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count(DISTINCT("Name of Class")) >= 4;
 --Pulls Career Development program of study
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Career Development Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Career Development Certificate"
 From
 	R_certificate_csv rc
 	JOIN
@@ -76,13 +87,15 @@ WHERE
 	('Public Speaking Pt. 1', 'Public Speaking Pt. 2', 'Resume Writing', 'SMART Goals Pt. 1', 
 	'SMART Goals Pt. 2', 'Time Management Pt. 1', 'Time Management Pt. 2', 'Managing Difficult Conversations Pt. 1',
 	'Managing Difficult Conversations Pt. 2')
+	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
 	Count(DISTINCT("Name of Class")) >= 9;
 --Pulls Career Exploration program of study
 SELECT 
-	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, "Career Development Certificate"
+	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Career Development Certificate"
 From
 	R_certificate_csv rc
 	JOIN
@@ -92,6 +105,8 @@ WHERE
 	('Public Speaking Pt. 1', 'Public Speaking Pt. 2', 'Resume Writing', 'SMART Goals Pt. 1', 
 	'SMART Goals Pt. 2', 'Time Management Pt. 1', 'Time Management Pt. 2', 'Managing Difficult Conversations Pt. 1',
 	'Managing Difficult Conversations Pt. 2')
+	 AND
+ 	rp."Receptive to Email" ="Yes"
 GROUP BY 
 	"Parent Information" 
 HAVING 
