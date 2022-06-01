46a88044a1eebb9a33900bc2ad9bf2303bd2c4ae
DerikVo <derikvosanjose@gmail.com>
2022-06-01 15:40:50 -0700

    Updated query logic and conditions
    Removed the 'greater or equal' than in having statement, adding the distinct feature should no longer count participants taking multiple of a single class in the "IN statement" as a clear condition.
    
    Added a date condition for dates past 12/31/2022 since stakeholder wants a report for 2021 onward.

--- SQL/CertificateQueries.sql
+++ SQL/CertificateQueries.sql
@@ -8,12 +8,14 @@ From
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE 
  	"Name of Class" IN ("Zoom-1", "Zoom-2")
- 	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 2;
+	Count(DISTINCT("Name of Class")) = 2;
 --Pulls Google Drive Certificate
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Google Drive Certificate"
@@ -24,12 +26,14 @@ From
 WHERE 
  	"Name of Class" IN ('Google Docs Practice Session' , 'Google Drive Practice Session', 'Google Drive-1', 'Google Drive-2', 'Google Drive-3 Pt. 1' , 'Google Drive-3 Pt. 1', 
  	'Google Drive-3 Pt. 2', 'Google Drive-4')
- AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 7;
+	Count(DISTINCT("Name of Class")) = 7;
 --Pulls Typing Certificate
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Typing Certificate"
@@ -39,12 +43,14 @@ From
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE 
  	"Name of Class" IN ('Typing-1')
- 	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 1;
+	Count(DISTINCT("Name of Class")) = 1;
 --Pulls LinkedIn Certificate
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "LinkedIn Certificate"
@@ -54,12 +60,14 @@ From
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE
 	"Name of Class" IN ('LinkedIn-1', 'LinkedIn-2')
-	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count("Name of Class") >= 2;
+	Count("Name of Class") = 2;
 --Pulls Math Certificate
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Math Certificate"
@@ -69,12 +77,14 @@ From
 	R_parentInfo_csv rp on rc."Parent Information" = rp."Parent Academy ID" 
 WHERE
 	"Name of Class" IN ('Math-1 pt. 1', 'Math-1 pt. 2', 'Math-2 pt. 1', 'Math-1 pt. 2')
-	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 4;
+	Count(DISTINCT("Name of Class")) = 4;
 --Pulls Career Development program of study
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Career Development Certificate"
@@ -87,12 +97,14 @@ WHERE
 	('Public Speaking Pt. 1', 'Public Speaking Pt. 2', 'Resume Writing', 'SMART Goals Pt. 1', 
 	'SMART Goals Pt. 2', 'Time Management Pt. 1', 'Time Management Pt. 2', 'Managing Difficult Conversations Pt. 1',
 	'Managing Difficult Conversations Pt. 2')
-	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 9;
+	Count(DISTINCT("Name of Class")) = 9;
 --Pulls Career Exploration program of study
 SELECT 
 	DISTINCT(rc."Parent Information") as ID, rp."First Name" || " " || rp."Last Name" as Name, rp."Email", "Career Exploration Certificate"
@@ -103,9 +115,11 @@ From
 WHERE
 	"Name of Class" IN 
 	('Career Exploration Program of Study Pt. 1', 'Career Exploration Program of Study Pt. 2', 'Career Exploration Program of Study Pt. 3')
-	 AND
+	AND
  	rp."Receptive to Email" ="Yes"
+	AND
+ 	Date > 12/31/2020
 GROUP BY 
 	"Parent Information" 
 HAVING 
-	Count(DISTINCT("Name of Class")) >= 3;
+	Count(DISTINCT("Name of Class")) = 3;
