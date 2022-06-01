394299068a2bd6f4e99940f22ea8243c55c544bb
DerikVo <derikvosanjose@gmail.com>
2022-06-01 16:29:10 -0700

    Utilized joins
    Used joined to get more detailed information about  parents who earned a single certificate and how to contact them.

--- SQL/IndividualClassCertificates.sql
+++ SQL/IndividualClassCertificates.sql
@@ -1,7 +1,15 @@
 --Certificates earned for taking individual classes
 SELECT 
-	"Parent Information", "Parent Name" , "Name of Class"
+	info."Parent Academy ID", info."First Name" || " " || info."Last Name" as Name , cert."Name of Class", info.Email 
 From
-	R_certificate_csv rcc
+	R_certificate_csv cert
+	JOIN
+	R_parentInfo_csv info
+	ON
+	cert."Parent Information" = info."Parent Academy ID" 
+WHERE 
+	Date >12/31/2020
+	AND
+	info."Receptive to Email" = "Yes"
 Order BY
 	"Name of Class", "Parent Information" DESC
