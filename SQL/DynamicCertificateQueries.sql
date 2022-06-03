fa9aed8fdc85721a3899375f72fc8ba9f9f5e738
DerikVo <derikvosanjose@gmail.com>
2022-06-03 14:21:12 -0700

    Discovered issue with table "TestList"
    Printed class name and found subquery was pulling all elements in the table. For now the only solution is to have class certificates separated into different tables.

--- SQL/DynamicCertificateQueries.sql
+++ SQL/DynamicCertificateQueries.sql
@@ -1,13 +1,12 @@
 /*
 Created a more dynmaic approch to pulling reports
 Next steps is to use user input and case conditions to pull reports
-
 20220603
 */
 
 SELECT 
-	info."Parent Academy ID", info."First Name" || " " || info."Last Name" as Name , 
-	'Zoom certificate', --title of certificate, will try to make this dynamic with user input
+	info."Parent Academy ID", info."First Name" || " " || info."Last Name" as Name, 
+	cert."Name of Class" , --title of certificate, will try to make this dynamic with user input
 	info.Email 
 From
 	R_certificate_csv cert
@@ -16,22 +15,18 @@ From
 	ON
 	cert."Parent Information" = info."Parent Academy ID"
 	CROSS JOIN 
-	TestList_csv tl --small list of a few classes to check to see if method works, will add other classes to the CSV
+	zoomcsv_csv zc --small list of a few classes to check to see if method works, will add other classes to the CSV
 WHERE
 	cert."Name of Class" 
 	IN(
 		SELECT
-			tl."Zoom"
+			zc.Zoom
 		FROM
-			TestList_csv tl)  --checks what clases are required to earn the zoom certificate
+			zoomcsv_csv zc --Changed to single table for each certificate, having multiple certificates and classes was casuing issues with pulling the wrong class.
+			)
 GROUP BY
-	info."Parent Academy ID" 
+	info."Parent Academy ID"
 HAVING
-	COUNT(DISTINCT(tl.Zoom)) = COUNT(DISTINCT(cert."Name of Class")) --only counts unique classes in list and ignores duplicate classes parents have taken
-	
-	/* Adding multiple coloumns to the TestList csv causes this function to not pull any data.
-	I assume its because its pulling all the elements in the table, and not just the coloumn.
-	We would need to create seperate tables for each certificate and use case statments to pull
-	the necessary data if we want a dynamic sql query. */
+	COUNT(DISTINCT(zc.Zoom)) = COUNT(DISTINCT(cert."Name of Class")) 
 Order BY
-	"Parent Information"
+	info."Parent Academy ID"
