4e2b1c3dad5b58f63e2d0a6ed037247a4163f941
DerikVo <derikvosanjose@gmail.com>
2022-07-29 10:18:26 -0700

    Added more details
    Added more details to send a mail merge of certificates to students. Changed the date to a format that the system recognizes.

--- SQL/Certificates/IndividualClassCertificates.sql
+++ SQL/Certificates/IndividualClassCertificates.sql
@@ -1,6 +1,11 @@
 --Certificates earned for taking individual classes
 SELECT 
-	info."Parent Academy ID", info."First Name" || " " || info."Last Name" as Name , cert."Name of Class", info.Email 
+	Distinct(TRIM(cert."Name of Class")) AS "Name of Class", --Identifying unique classes, but because some repeats have differnet instructors duplicates will remain remain.
+	info."Parent Academy ID",
+	info."first Name" AS "First Name", --added a seperate column for first name, so mail merge will refer to them by their first name instead of their full name.
+	info."First Name" || " " || info."Last Name" as "Full Name", --Full name is used for labeling the certificates
+	info.Email, cert."Date",
+	cert."Instructor" -- Provides information on who will present the certificates to students
 From
 	R_certificate_csv cert
 	JOIN
@@ -8,7 +13,7 @@ From
 	ON
 	cert."Parent Information" = info."Parent Academy ID" 
 WHERE 
-	Date >12/31/2020
+	Date > "2020-12-31" --Changed date format, system only recognizes dates stored in YYYY-MM-DD
 	AND
 	info."Receptive to Email" = "Yes"
 Order BY
